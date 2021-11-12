89a2c8c45c5989c883c130592ed3386b46b7b899
LeoZ06 <79598689+LeoZ06@users.noreply.github.com>
2021-11-12 17:20:19 +0000

    Solved problems.

--- Program1.py
+++ Program1.py
@@ -2,7 +2,11 @@
 *********
 PROGRAM 1
 *********
-Define a function number_of_odds that takes a list of integers as an argument. The function returns how an integer, how many odd numbers are in the list.
+Define a function number_of_odds that takes a list of integers as an argument. The function returns how many odd numbers are in the list.
 '''
 def number_of_odds(lst):
-  pass #delete this line when you start writing your code
+  ans = 0
+  for i in lst:
+    if i % 2 == 0:
+      ans = ans + 1
+  return(ans)
